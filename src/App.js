002a39490f8594e7753478cdf6389b71bcbfde31
zahidhasan125 <zahidhasan125@gmail.com>
2023-03-30 19:38:57 +0600

    tailwind and flowbite install, folder stucture created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { router } from './routes/Routes';
 
 function App() {
   return (
-    <div>
-      Hello admin!
+    <div className='text-white bg-gradient-to-bl from-cyan-900 via-sky-900 to-blue-900 min-h-screen'>
+      <RouterProvider router={router} ></RouterProvider>
     </div>
   );
 }
---
+++ src/layouts/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            Main layout
+        </div>
+    );
+};
+
+export default Main;
